5a15f2b70ced021ba60280dab1b00b88b955a1d3
Sravani294 <86480700+Sravani294@users.noreply.github.com>
2022-12-04 10:46:20 +0000

    FbSample

---
+++ HomeController.cs
@@ -0,0 +1,43 @@
+﻿using FootballLep.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FootballLep.Controllers
+{
+    public class HomeController : Controller
+    {
+        MyDbContext context = new MyDbContext();
+        // GET: Home
+        public ActionResult Index1()
+        {
+            return View(context.Matches);
+
+
+        }
+        public ActionResult add()
+
+        {
+
+            return View();
+
+        }
+        public ActionResult Addnewmatchdetails(Match a)
+
+        {
+
+            context.Matches.Add(a);
+
+            context.SaveChanges();
+
+            return RedirectToAction("Index");
+
+        }
+
+
+
+    }
+}
---
+++ Match.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace FootballLep.Models
+{
+    public class Match
+    {
+        [Key]
+
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int MatchID { get; set; }
+        public String TeamName1 { get; set; }
+        public String Teamname2 { get; set; }
+        public String StatusTeam {get; set;}
+        public String WinningTeam { get; set; }
+    }
+}
